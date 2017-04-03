2c83e1b1c1e01b773e71c053922566c3690434c5
Irl Nathan <irlnathan@gmail.com>
2017-04-03 10:39:21 -0500

    added different component creation and dependency injection examples

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,5 +1,10 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+// var React = require('react');
+// var Component = require('react').Component;
+// var ReactDOM = require('react-dom');
+
+// You could use es6 module syntax instead
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 // Require the styles for this page.  This will cause the `homepage.bundle.css` file
 // to be generated.  Note that you can require multiple .less or .css files here, and
@@ -11,15 +16,36 @@ require('../styles/homepage.less');
 // do things like `io.socket.get()` inside of this script.
 var io = require('../dependencies/sockets.js');
 
-var SailsReact = React.createClass({
-  render: function() {
+// Using a class to create a component
+export default class SailsReact extends Component {
+  render() {
     return (
       <div>
         <h1>Hello Sails-React!</h1>
       </div>
     );
   }
-});
+}
+
+// Using createClass to create Component
+// var SailsReact = React.createClass({
+//   render: function() {
+//     return (
+//       <div>
+//         <h1>Hello Sails-React!</h1>
+//       </div>
+//     );
+//   }
+// });
+
+// Function React Component
+// const SailsReact = () => {
+//   return (
+//     <div>
+//       <h1>Hello Sails-React!</h1>
+//     </div>
+//   )
+// };
 
 ReactDOM.render(
   <SailsReact/>,
